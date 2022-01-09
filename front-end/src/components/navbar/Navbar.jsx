b98b1ced506f3a485560f6c6694cdbf9d4c27d3c
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2022-01-09 11:14:12 +0700

    navbar

---
+++ front-end/src/components/navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import { useState } from "react";
+import "./navbar.scss"
+
+const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return() => (window.onscroll == null);
+    };
+
+    return (
+        <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+            <div className="container">
+                <div className="left">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="logo"/>
+                    <span>Homepage</span>
+                    <span>Series</span>
+                    <span>Movies</span>
+                    <span>New and Popular</span>
+                    <span>My List</span>
+                </div>
+
+                <div className="right">
+                    <Search className="icon"/>
+                    <span>KID</span>
+                    <Notifications className="icon"/>
+                    <img src="https://1.bp.blogspot.com/-1RUvDLOggXQ/XszB_FEn3wI/AAAAAAAAmLs/T_GOYrIeDuMFwt-Gwz3nKobP7XF36c2NwCLcBGAsYHQ/s1600/Anh-gai-xinh-deo-kinh%2B%252861%2529.jpg" alt=""/>
+                    
+                    <div className="profile">
+                        <ArrowDropDown className="icon"/>
+                        <div className="options">
+                            <span>Settings</span>
+                            <span>Logout</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
