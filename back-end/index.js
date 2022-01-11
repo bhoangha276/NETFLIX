24891af1ab894f142dc106465f7ce6205dd1a061
Alex <alexnguyen123229@gmail.com>
2022-01-11 20:48:38 +0700

    updateUser

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+const authRoute = require("./routes/auth")
 dotenv.config();
 
 main().catch(err => console.log(err));
@@ -11,6 +11,10 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URL);
   console.log("DB connection successfuly!");
 }
+
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
