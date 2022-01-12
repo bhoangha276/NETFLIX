c15a9ed135bca6f21a76208d8cc192b5e12893a6
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2022-01-12 21:43:31 +0700

    CRUD movie & list

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoute = require("./routes/auth")
-const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
 dotenv.config();
 
 main().catch(err => console.log(err));
@@ -17,6 +20,8 @@ async function main() {
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
