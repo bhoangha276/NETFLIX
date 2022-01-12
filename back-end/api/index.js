a903416dcdf49ed05356977d2610a530dcc6771e
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2022-01-12 20:08:05 +0700

    CRUD user

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
 dotenv.config();
 
 main().catch(err => console.log(err));
@@ -15,6 +16,7 @@ async function main() {
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
