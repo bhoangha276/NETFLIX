1039c81579e9c59118b26233ddc9ffc42b62a9d9
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2022-01-18 08:35:21 +0700

    fix

--- api/index.js
+++ api/index.js
@@ -23,6 +23,8 @@ app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT || 8800, (err) => {
+  if (err) throw err;
+  
   console.log("Backend server is running!");
 });
