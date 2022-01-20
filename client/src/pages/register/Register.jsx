1ae5428c2bbbeb20f6798b5262b49a9e3b1b80d3
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2022-01-20 10:29:40 +0700

    command

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useRef } from "react";
 import { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./register.scss";
 
 export default function Register() {
@@ -35,7 +35,9 @@ export default function Register() {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
           />
-          <button className="loginButton">Sign In</button>
+          {/* <Link to="/login"> */}
+            <button className="loginButton">Sign In</button>
+          {/* </Link> */}
         </div>
       </div>
       <div className="container">
